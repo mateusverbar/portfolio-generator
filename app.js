6d59047e11e32925bf5f82fa50bfbe89ad5d27bb
Matthew Barros <matthewbarros@Matthews-MBP.lan1>
2021-10-09 23:06:42 -0500

    validation

--- app.js
+++ app.js
@@ -5,12 +5,26 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: gitHubUserName => {
+                if (gitHubUserName) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -33,12 +47,26 @@ const promptProject = portfolioData => {
         {
             type:'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: projectName => {
+                if (projectName) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDescription => {
+                if (projectDescription) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -49,7 +77,14 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: gitLink => {
+                if (gitLink) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
